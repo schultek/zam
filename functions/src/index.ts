3ba7fe33f7d04d40f859b6e7316e6f40ae30d867
Kilian Schulte <schultek@in.tum.de>
2020-10-27 17:48:30 +0100

    update functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,19 +3,48 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-export const updateUserRole = functions.https.onCall(async (data, context) => {
+const db = admin.firestore();
+db.settings({timestampsInSnapshots: true});
+
+export const updateUserPermissions = functions.https.onCall(async (data, context) => {
 
   if (!context.auth) {
     throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
   }
-  if (!data.role || !["organizer", "leader", "participant"].includes(data.role)) {
-    throw new functions.https.HttpsError('failed-precondition', 'The function must be called with a valid role.');
+  if (data.canCreateTrips == null || typeof data.canCreateTrips != "boolean") {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called with a valid permission key.');
   }
 
-  const role = data.role; // "organizer" | "leader" | "participant"
   const uid = context.auth.uid;
 
-  console.log(`Setting role of user ${uid} to '${role}'.`);
+  console.log(`Setting permission of user ${uid} to 'canCreateTrips=${data.canCreateTrips}'.`);
 
-  await admin.auth().setCustomUserClaims(uid, {role: role});
+  await admin.auth().setCustomUserClaims(uid, {canCreateTrips: data.canCreateTrips});
 });
+
+export const addUserToTrip = functions.https.onCall(async (data, context) => {
+
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+  }
+  if (!data.tripId) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called with a valid tripId.');
+  }
+  if (!data.userRole) {
+    data.userRole = "participant";
+  }
+
+  const tripId = data.tripId;
+  const userRole = data.userRole;
+  const uid = context.auth.uid;
+
+  let trip = await db.collection("trips").doc(tripId).get();
+
+  if (!trip.exists) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called with a valid tripId.');
+  }
+
+  console.log(`Adding user ${uid} to trip ${tripId} with role ${userRole}.`);
+
+  await trip.ref.update('users.'+uid+'.role', userRole);
+})
