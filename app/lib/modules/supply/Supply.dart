22668305bd40d60d9250c7509f4567921938d229
Kilian Schulte <schultek@in.tum.de>
2020-11-08 22:00:26 +0100

    add cooking pot

--- app/lib/modules/supply/Supply.dart
+++ app/lib/modules/supply/Supply.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:jufa/general/Module.dart';
+import 'package:rive/rive.dart';
 
 import 'CookingPage.dart';
 import 'ShoppingPage.dart';
@@ -13,7 +15,17 @@ class Supply extends Module {
         builder: (context) => GestureDetector(
           child: Container(
             padding: EdgeInsets.all(10),
-            child: Center(child: Text("Kochen")),
+            child: Stack(
+              children: [
+                Positioned.fill(child: CookingPot()),
+                Positioned(
+                  top: 8,
+                  left: 0,
+                  right: 0,
+                  child: Center(child: Text("Kochen", style: TextStyle(color: Colors.black45))),
+                )
+              ],
+            ),
           ),
           onTap: () {
             Navigator.of(context).push(MaterialPageRoute(builder: (context) => CookingPage()));
@@ -24,7 +36,7 @@ class Supply extends Module {
         builder: (context) => GestureDetector(
           child: Container(
             padding: EdgeInsets.all(10),
-            child: Center(child: Text("Kochen")),
+            child: Center(child: Text("Einkaufen")),
           ),
           onTap: () {
             Navigator.of(context).push(MaterialPageRoute(builder: (context) => ShoppingPage()));
@@ -34,5 +46,37 @@ class Supply extends Module {
     ];
   }
 
+}
+
+class CookingPot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Artboard>(
+      future: rootBundle.load('lib/assets/animations/cookingpot.riv').then((data) async {
+        var file = RiveFile();
+        file.import(data);
+
+        var artboard = file.mainArtboard;
+
+        artboard.addController(SimpleAnimation("Stir"));
+        artboard.addController(SimpleAnimation("Smoke"));
+
+        artboard.originY = -0.12;
+
+        return artboard;
+      }),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Rive(
+            artboard: snapshot.data,
+            fit: BoxFit.cover,
+            alignment: Alignment.bottomCenter,
+          );
+        } else {
+          return Container();
+        }
+      },
+    );
+  }
 }
 
